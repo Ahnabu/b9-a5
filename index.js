deaa7f19d2c649f7e987a30d4480ed4ac0b57d1f
Ahnabu <horairaabu013025@gmail.com>
2024-02-18 17:05:16 +0600

    seat started

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('index connected')
+let seatTotal = 40;
+let selectedSeat = 0;
+let seatLeft = document.getElementById('seat-left');
+let selectSeat = document.getElementById('Selected-seat')
+const seats = document.querySelectorAll(".bus-btn");
+    
+function createSeat(seatName) {
+    let seatContainer = document.getElementById("select-seat");
+    let seat = document.getElementById('seat').innerText = seatName;
+    let classN = document.getElementById('class').innerText = "Business Class";
+    let price = document.getElementById('price').innerText = "BDT-550";
+    let newSeat = document.createElement('p');
+    newSeat.appendChild(seat);
+    let newClass = document.createElement('p');
+    newClass.appendChild(classN);
+    let newPrice = document.createElement('p');
+    newSeat.appendChild(price);
+
+    
+
+}
+    for (const seat of seats) {
+        
+        seat.addEventListener('click', function () {
+            
+            seat.style.backgroundColor = "#1DD100";
+            seatTotal = seatTotal - 1;
+            console.log(seatTotal);
+            seatLeft.innerText = seatTotal;
+            selectedSeat = selectedSeat + 1;
+            selectSeat.innerText = selectedSeat;
+            createSeat(seat);
+        })
+    }
